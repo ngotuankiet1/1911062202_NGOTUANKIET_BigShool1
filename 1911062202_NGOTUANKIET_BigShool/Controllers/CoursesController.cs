a69dd9474d66efd9ae27560ed34b83f179ede35c
Asus <Asus@DESKTOP-7VDO8JQ>
2021-06-30 16:43:23 +0700

    Add project files.

---
+++ 1911062202_NGOTUANKIET_BigShool/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911062202_NGOTUANKIET_BigShool.Controllers
+{
+    
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ 1911062202_NGOTUANKIET_BigShool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911062202_NGOTUANKIET_BigShool.MvcApplication" Language="C#" %>
