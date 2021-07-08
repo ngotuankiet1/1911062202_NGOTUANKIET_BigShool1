72062a83682699bed501310325b62c7e6b373c60
Asus <Asus@DESKTOP-7VDO8JQ>
2021-07-08 22:40:50 +0700

    trang 54

--- 1911062202_NGOTUANKIET_BigShool/Controllers/CoursesController.cs
+++ 1911062202_NGOTUANKIET_BigShool/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using _1911062202_NGOTUANKIET_BigShool.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -50,6 +51,40 @@ namespace _1911062202_NGOTUANKIET_BigShool.Controllers
             _dbContext.SaveChanges();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var courses = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = courses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.Datetime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+
+            return View(courses);
         }
     }
 }
